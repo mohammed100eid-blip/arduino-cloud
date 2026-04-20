79b4587778f820b5d4d069ddf326e02db63ff3a4
mohammed <mohammed100eid@gmail.com>
2026-04-21 00:50:08 +0200

    Implement Express server for Arduino code compilation
    Added an Express server with a /compile endpoint to compile Arduino code.

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
+const express = require("express")
+const { exec } = require("child_process")
+const fs = require("fs")
 
+const app = express()
+
+app.use(express.json())
+
+app.post("/compile",(req,res)=>{
+
+const code = req.body.code
+
+fs.writeFileSync("sketch.ino",code)
+
+exec("arduino-cli compile --fqbn arduino:avr:uno .",(err,stdout,stderr)=>{
+
+if(err){
+res.send(stderr)
+return
+}
+
+const hex = fs.readFileSync("build/arduino.avr.uno/sketch.ino.hex")
+
+res.send(hex)
+
+})
+
+})
+
+app.listen(3000,()=>{
+console.log("server running")
+})
